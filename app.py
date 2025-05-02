77d456152bfe6346529b7be3ea4e20e478104619
yamakenzzr <yamaken.zzr1200@gmail.com>
2025-05-03 07:40:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def terms():
     return render_template('terms.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
